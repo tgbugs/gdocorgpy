60c7ba0eaaa3e9abe53e4b8ea61bfe7e09383bb9
Tom Gillespie <tgbugs@gmail.com>
2019-03-11 00:23:20 -0700

    drive v1 api -> org mode converter woo

--- scripts/drive.py
+++ scripts/drive.py
@@ -34,6 +34,217 @@ def get_oauth_service(store_file,
     return service
 
 
+def get_docs_service(store_file):
+    DISCOVERY_DOC = 'https://docs.googleapis.com/$discovery/rest?version=v1'
+    store = file.Storage((spath / store_file).as_posix())
+    creds = store.get()
+    service = build('docs', 'v1', http=creds.authorize(Http()),
+                    discoveryServiceUrl=DISCOVERY_DOC)
+    return service
+
+class Convert:
+    mapping = (
+        (('paragraph'),),
+        (('paragraph'),),
+    )
+
+class OrgDoc:
+    def __init__(self, org):
+        self.o = org
+
+class DocOrg:
+    def __init__(self, docs):
+        self.docs = docs
+
+    def json(self, doc_name):
+        return self.docs.get_doc_json(doc_name)
+
+    def __call__(self, doc_name, start_heading=''):
+        self.j = self.json(doc_name)
+        self.start_heading = start_heading
+        org = ''
+        self.indent = ''
+        self.in_table = False
+        self.stack = []
+        elements = self.j['body']['content']
+        nexts = elements[1:] + [None]
+        for element, nxt in zip(elements, nexts):
+            e = self.element(element)
+            if nxt is not None:
+                # peeking to see if we need to strip trailing whitespace
+                if 'paragraph' in nxt and nxt['paragraph']['paragraphStyle']['namedStyleType'] != 'NORMAL_TEXT':
+                    e = e.rstrip(' ')  # we have a header!
+
+            org += e
+
+        self.j = None
+        self.start_heading = ''
+        return org
+
+    def element(self, element):
+        # start index and end index are probably useful ...
+        # esp for crdt type updates
+        types = 'table', 'paragraph', 'sectionBreak', 'tableOfContents'
+        for t in types:
+            if t in element:
+                return getattr(self, t)(element[t])
+        else:
+            print(element.keys())
+
+    def table(self, table):
+        sep = '|'
+        out = ''
+        self.in_table = True
+        for row in table['tableRows']:
+            for cell in row['tableCells']:
+                out += sep + ' '
+                for element in cell['content']:
+                    content = self.element(element)
+                    content = content.replace('\n', '')  # FIXME can newline but hard
+                    out += content + ' '
+
+            out += sep + '\n' + self.indent
+
+        self.in_table = False
+        return out
+
+    def paragraph(self, paragraph):
+        #print(self.stack)
+        mapping = {
+            'NORMAL_TEXT': '',
+            'TITLE': '* ',
+            'HEADING_1': '** ',
+            'HEADING_2': '*** ',
+            'HEADING_3': '**** ',
+            'HEADING_4': '***** ',
+        }
+        out = ''
+
+        style = paragraph['paragraphStyle']['namedStyleType']
+        head = mapping[style]
+        lh = len(head)
+        if lh:
+            head = self.start_heading + head
+            lh = len(head)
+            lsh = len(self.start_heading)
+            self.indent = ' ' * lh
+            while self.stack and lh <= len(mapping[self.stack[-1]]) + lsh:
+                o_head = self.stack.pop(-1)
+        
+            self.stack.append(style)
+
+            out += head
+
+        if 'bullet' in paragraph:
+            bullet = paragraph['bullet']
+            listId = bullet['listId']
+            lst = self.j['lists'][listId]
+            nls = lst['listProperties']['nestingLevels']
+            if 'glyphType' in nls[0]:
+                symbol = '1. '
+            elif 'glyphSymbol' in nls[0]:
+                symbol = '- '
+
+            if 'nestingLevel' in bullet:
+                nestingLevel = bullet['nestingLevel']
+            else:
+                nestingLevel = 0
+
+            bhead = (nestingLevel * 2 * ' ')  + symbol
+        else:
+            bhead = ''
+
+        for element in paragraph['elements']:
+            e = self.paragraph_element(element)
+            if e.strip():  # issue where bhead appended when last element is empty!?
+                e = bhead + e
+            out += e
+
+        return out
+
+    def paragraph_element(self, element):
+        types = 'textRun', 'inlineObjectElement'
+        for t in types:
+            if t in element:
+                return getattr(self, t)(element[t])
+
+    def inlineObjectElement(self, ioe):
+        oid = ioe['inlineObjectId']
+        iobj = self.j['inlineObjects'][oid]
+        eobj = iobj['inlineObjectProperties']['embeddedObject']
+        types = 'imageProperties', 'embeddedDrawingProperties'
+        for t in types:
+            if t in eobj:
+                obj = eobj[t]
+                if obj:
+                    uri = obj['contentUri']
+                    return f'[[{uri}]]'
+                else:
+                    return f'[[{oid}][Missing embedded object!]]'
+
+        else:
+            raise TypeError(f'Unknown type in {list(eobj.keys())}')
+
+    def textRun(self, tr):
+        ts = tr['textStyle']
+        styles = 'underline', 'bold', 'italic', 'strikethrough'
+        lt = '' if self.in_table else '\n' + self.indent
+        mapping = {
+            'underline': '_',
+            'bold': '*',
+            'italic': '/',
+            'strikethrough': '+',  # TODO haven't seen this yet
+            'line-terminator': lt,
+        }
+        stack = []
+        out = ''  # FIXME reverse whitespace ...
+
+        content = tr['content']
+        while content.endswith('\n'):
+            stack.append('line-terminator')
+            content = content[:-1]
+
+        for style in styles:
+            if style in ts:
+                if style == 'underline' and 'link' in ts:
+                    style = 'link'
+                    href = ts['link']['url']
+                    mapping['link'] = f'[[{href}]['
+
+                out += mapping[style]
+                stack.append(style)
+
+        out += content
+
+        while stack:
+            style = stack.pop(-1)
+            if style == 'link':
+                out += ']]'
+            else:
+                out += mapping[style]
+
+        return out
+
+    def sectionBreak(self, value):
+        return '\n'
+
+    def tableOfContents(self, value):
+        return ''
+
+           
+class Docs:
+    def __init__(self, store_file, converter=DocOrg):
+        self.service = get_docs_service(store_file)
+        self.converter = converter(self)
+
+    def get_doc_json(self, doc_name):
+        file_id = devconfig.secrets('google', 'docs', doc_name)
+        return self.service.documents().get(documentId=file_id,
+                                            suggestionsViewMode='SUGGESTIONS_INLINE').execute()
+
+    def get_doc_org(self, doc_name, start_heading='**'):
+        return self.converter(doc_name, start_heading=start_heading)
+
 class Drive:
     def __init__(self, store_file):
         self.service = get_oauth_service(store_file)
